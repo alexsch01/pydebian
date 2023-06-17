bcfd36ee39a23ca01e77520a207907f37f8c62c4
Alex Schwartz <alexschwartz01@gmail.com>
2023-06-17 11:02:28 -0400

    Update 2.7.sh

--- 2.7.sh
+++ 2.7.sh
@@ -1,12 +1,13 @@
 sudo apt update
-sudo apt install -y wget make
+sudo apt install -y wget make binutils
 wget https://github.com/alexsch01/pydebian/releases/download/python2.7/Python-2.7.tar.xz
 tar -xf Python-2.7.tar.xz
 cd Python-2.7
 sudo make altinstall
 cd ..
 sudo rm -rf Python-2.7*
-# sudo rm /usr/local/bin/2to3-3.11
-# sudo rm /usr/local/bin/idle3.11
-# sudo rm /usr/local/bin/pip3.11
-# sudo rm /usr/local/bin/python3.11-config
+sudo rm /usr/local/bin/2to3
+sudo rm /usr/local/bin/idle
+sudo rm /usr/local/bin/pydoc
+sudo rm /usr/local/bin/python2.7-config
+sudo rm /usr/local/bin/smtpd.py
